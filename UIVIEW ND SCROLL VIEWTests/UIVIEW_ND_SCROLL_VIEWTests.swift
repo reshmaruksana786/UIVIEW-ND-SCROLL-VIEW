85a334728329a40311b9d3b9eaa6f99bc10bb122
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-09-23 13:31:01 +0530

    Initial Commit

---
+++ UIVIEW ND SCROLL VIEWTests/UIVIEW_ND_SCROLL_VIEWTests.swift
@@ -0,0 +1,34 @@
+//
+//  UIVIEW_ND_SCROLL_VIEWTests.swift
+//  UIVIEW ND SCROLL VIEWTests
+//
+//  Created by Syed.Reshma Ruksana on 23/09/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import XCTest
+@testable import UIVIEW_ND_SCROLL_VIEW
+
+class UIVIEW_ND_SCROLL_VIEWTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
